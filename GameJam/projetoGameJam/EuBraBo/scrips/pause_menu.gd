66bdaf573d0be664fd9863046fb29f9b748f2336
Eu Bra Bo48 <nicknicolas025@gmail.com>
2024-07-26 14:58:31 -0300

    menu_pause
    aqui ja está o pause e quit

---
+++ GameJam/projetoGameJam/EuBraBo/scrips/pause_menu.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+onready var resume_btn = $menu_holder/resume_btn
+
+
+func _ready():
+	visible = false
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		resume_btn.grab_focus()
+
+func _on_resume_btn_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
